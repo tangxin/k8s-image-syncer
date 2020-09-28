c249878d9c90bd966408c8ddda2870869bd710d6
tangx <uyinn@live.com>
2020-09-28 16:47:08 +0800

    ci: add auto udpate readme.md

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# k8s-image-syncer
+# k8s image syncer
+sync image to your repo for kubernetes
+
+
++ [kubeimages/coredns:1.7.0](dockerfiles/coredns.Dockerfile)
++ [kubeimages/flannel:v0.13.0](dockerfiles/flannel.Dockerfile)
++ [kubeimages/kubernetesui-dashboard:v2.0.0](dockerfiles/kubernetesui-dashboard.Dockerfile)
++ [kubeimages/metrics-server:v0.3.7](dockerfiles/metrics-server.Dockerfile)
++ [kubeimages/nginx-ingress-controller:v0.25.1](dockerfiles/nginx-ingress-controller.Dockerfile)
++ [kubeimages/pause:3.1](dockerfiles/pause.Dockerfile)
--- sync.sh
+++ sync.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-set -x
+
+cat > README.md <<EOF
+# k8s image syncer
+sync image to your repo for kubernetes
+
+EOF
+
 for dockerfile in $(ls dockerfiles); do
     {
         _image=${dockerfile/.Dockerfile/}
         image=${_image%%,*}
         # tag=${_image##*,}
         tag=$(grep FROM dockerfiles/${dockerfile}  | head -n 1 | awk -F'[: ]' '{print $3}')
-        
 
         docker buildx build --push --platform=linux/amd64,linux/arm64 \
             --tag=kubeimages/${image}:${tag} \
             --file=dockerfiles/${dockerfile} \
             --build-arg=TAG=${tag} .
+
+        echo "+ [kubeimages/${image}:${tag}](dockerfiles/${dockerfile})" | tee -a README.md
 
     }
 done
