aa22743e4fc708e8ab473179b4251731c5161eed
tangx <uyinn@live.com>
2020-10-10 14:07:44 +0800

    ci: workflow in develop

--- sync.sh
+++ sync.sh
@@ -1,28 +1,33 @@
 #!/bin/bash
 
 
-cat > README.md <<EOF
-# k8s image syncer
-sync image to your repo for kubernetes
+# cat > README.md <<EOF
+# # k8s image syncer
+# sync image to your repo for kubernetes
 
-## $(date +%F)
+# ## $(date +%F)
 
-EOF
+# EOF
 
-for dockerfile in $(ls dockerfiles); do
+TARGET=$1
+[ -z ${TARGET} ] && exit 1
+
+
+for dockerfile in $(ls ${TARGET}); do
     {
-        _image=${dockerfile/.Dockerfile/}
+        # _image=${dockerfile/.Dockerfile/}
+        _image=$(basename ${TARGET})
         image=${_image%%,*}
         # tag=${_image##*,}
-        tag=$(grep FROM dockerfiles/${dockerfile}  | head -n 1 | awk -F'[: ]' '{print $3}')
+        tag=$(grep FROM ${TARGET}/${dockerfile}  | head -n 1 | awk -F'[: ]' '{print $3}')
         [ -z "${tag}" ] && tag=latest
 
         docker buildx build --push --platform=linux/amd64,linux/arm64 \
             --tag=kubeimages/${image}:${tag} \
-            --file=dockerfiles/${dockerfile} \
+            --file=${TARGET}/${dockerfile} \
             --build-arg=TAG=${tag} .
 
-        echo "+ [kubeimages/${image}:${tag}](https://hub.docker.com/r/kubeimages/${image}/tags)" | tee -a README.md
+        # echo "+ [kubeimages/${image}:${tag}](https://hub.docker.com/r/kubeimages/${image}/tags)" | tee -a README.md
 
     }
 done
