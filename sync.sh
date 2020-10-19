bb7cf360ea66ada2b308e4719ac923b5183f7161
tangx <uyinn@live.com>
2020-10-19 10:51:39 +0800

    chore: no use selfhost

--- sync.sh
+++ sync.sh
@@ -3,7 +3,6 @@
 TARGET=$1
 [ -z ${TARGET} ] && exit 1
 
-
 for dockerfile in $(ls ${TARGET}); do
     {
         # _image=${dockerfile/.Dockerfile/}
@@ -13,10 +12,14 @@ for dockerfile in $(ls ${TARGET}); do
         tag=$(grep FROM ${TARGET}/${dockerfile} | head -n 1 | awk -F'[: ]' '{print $3}')
         [ -z "${tag}" ] && tag=latest
 
-        docker buildx build --push --platform=linux/amd64,linux/arm64 \
-            --tag=${REGISTRY}/${image}:${tag} \
-            --file=${TARGET}/${dockerfile} \
-            --build-arg=TAG=${tag} .            \
-            || exit $?
+        while :
+        do
+            {
+                docker buildx build --push --platform=linux/amd64,linux/arm64 \
+                    --tag=${REGISTRY}/${image}:${tag} \
+                    --file=${TARGET}/${dockerfile} \
+                    --build-arg=TAG=${tag} . && continue
+            }
+        done
     }
 done
